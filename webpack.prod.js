2caa0a59888d25eb382d82db21371ad2e5623a01
Zeusamon <zeionsoft@gmail.com>
2021-08-20 22:50:40 +0100

    primer commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -14,12 +14,15 @@ module.exports = {
     },
     module: {
         rules: [
-            { 
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                use: [
-                    'babel-loader'
-                ]
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             },
             {
                 test: /\.css$/,
